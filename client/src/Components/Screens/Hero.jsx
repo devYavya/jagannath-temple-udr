66db71feebff37bb7f91ca6518bfa8de74d595f4
devYavya <yavya.sharma21@gmail.com>
2025-05-06 18:06:31 +0530

    notificaiton

--- client/src/Components/Screens/Hero.jsx
+++ client/src/Components/Screens/Hero.jsx
@@ -39,7 +39,7 @@ function Hero() {
     const shloka = getDailyShloka();
     setDailyShloka(shloka);
     requestNotificationPermission();
-  }, []);
+  }, [requestNotificationPermission]);
 
   return (
     <div
